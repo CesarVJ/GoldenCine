4d9b7ba860e176e8f45502b73315457d4c9164c2
cesar valdez jacinto <cvj991@gmail.com>
2020-04-27 10:40:16 -0500

    Se agregaron commentarios al index

--- index.php
+++ index.php
@@ -35,8 +35,9 @@
 		<div class="contenedor">
 
 			<div class="tab-content" id="myTabContent">
-				<?php require_once("paginas/formularioRegistro.html") ?>
-				<div class="tab-pane fade show active" id="iniciar-sesion" aria-labelledby="iniciar-sesion-tab">
+				 <!--Aqui se invoca el formulario de registro-->
+				<?php require_once("paginas/formularioRegistro.html")?>
+				<div class="tab-pane fade show active" id="iniciar-sesion" aria-labelledby="iniciar-sesion-tab">					
 					<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"
 						class="formulario">
 						<div class="grupo-correo">
@@ -64,6 +65,8 @@
 			</div>
 		</div>
 	</div>
+
+	<!-- Aqui se lleva a cabo el proceso de inicio de sesión -->
 	<?php
 		session_start();
 		if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
@@ -78,22 +81,29 @@
 		include 'conexion.php';
 		$conexion = abrirConexion();
 		$correo = $contraseña = "";
+		$error_correo="";
+		$error_contraseña="";
 		//echo md5("123456789");
 		if($_SERVER["REQUEST_METHOD"] == "POST"){
-			if(empty(trim($_POST["correo"]))){
+			//Se valida que el campo de correno no este vacio
+			if(empty(trim($_POST["correo"]))){ 
+				$error_correo = "No se proporciono correo";
 				echo "<script type='text/javascript'>correoIncorrecto();</script>";				
 			}else{
 				$correo = trim($_POST["correo"]);
 			}
+			//Se valida que el campo de contraseña no este vacio
 			if(empty(trim($_POST["contraseña"]))){
+				$error_contraseña="No se proporciono contraseña";
 				echo "<script type='text/javascript'>contraseñaIncorrecta();</script>";				
 			}else{
 				$contraseña = trim($_POST["contraseña"]);
 			}
+			//Si no existen errores, se procede a verificar si es administrador o cliente
 			if(empty($error_correo) && empty($error_contraseña)){
-				if($correo == "root@gmail.com"){
+				if($correo == "root@gmail.com"){ //Es un administrador
 					$consulta = "select id_administrador, correo, contraseña from administrador where correo = ?";
-				}else{
+				}else{ // Es un cliente
 					$consulta = "select id_cliente, correo, contraseña from cliente where correo = ?";
 				}
 				if($statement = mysqli_prepare($conexion, $consulta)){
@@ -101,24 +111,24 @@
 					$param_correo = $correo;
 					if(mysqli_stmt_execute($statement)){
 						mysqli_stmt_store_result($statement);
-						if(mysqli_stmt_num_rows($statement) == 1){
+						if(mysqli_stmt_num_rows($statement) == 1){ //Si el usuario fu encontrado
 							mysqli_stmt_bind_result($statement, $id, $correo, $contraseña_md5);
 							if(mysqli_stmt_fetch($statement)){
-								if(md5($contraseña) == $contraseña_md5){
-									session_start();
+								if(md5($contraseña) == $contraseña_md5){ // Se valida que la contraseña guardada en la BD coincida con la proporcionada
+									session_start(); // Se inicia la sesion
 									$_SESSION["loggedin"] = true;
 									$_SESSION["id"] = $id;
 									$_SESSION["correo"] = $correo;
-                                    if($correo == "root@gmail.com"){
+                                    if($correo == "root@gmail.com"){ // Si es administrador, se le dirije a la Cartelera del Administrador
                                         header("location: paginas/carteleraAdmin.php");
-                                    }else{
+                                    }else{ // Si es cliente, se le dirije a la Cartelera del Cliente
                                         header("location: paginas/carteleraCliente.php");
                                     }
-								}else{
+								}else{ // Las contraseñas no coinciden
 									echo "<script type='text/javascript'>contraseñaIncorrecta();</script>";				
 								}
 							}
-						}else{
+						}else{ // EL usuario no existe
 							echo "<script type='text/javascript'>correoIncorrecto();</script>";				
 						}
 					}else{
